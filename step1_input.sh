424b3d4a74d11df1b992f2a4df04170c03309a2a
Chris Han <55549961+XiaoqingHan@users.noreply.github.com>
2026-06-26 22:48:29 +0200

    Update step1_input.sh

--- step1_input.sh
+++ step1_input.sh
@@ -1,4 +1,4 @@
 year=$(date +%Y)
 month=$(date +%m)
 echo "The input data comes from: "$year"-"$month
-cat /ldfssz1/ST_BIGDATA/USER/st_bigdata/outdumpinfo/$year"-"$month > out.txt
+cat /USER/$year"-"$month > out.txt
