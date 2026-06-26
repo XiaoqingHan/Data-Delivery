a26e064959d5078deee7d215fe696566f1844ab5
Chris Han <55549961+XiaoqingHan@users.noreply.github.com>
2026-06-26 22:34:21 +0200

    Update script paths in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # get input
-sh /jdfssz1/ST_BIGDATA/Stereomics/USER/hanxiaoqing/data_deliv/v3/inpython/step1_input.sh
+sh /hanxiaoqing/data_deliv/v3/inpython/step1_input.sh
 # get output
-/ldfssz1/ST_BIGDATA/PMO/SOFTWARE/Python-3/bin/python3 /jdfssz1/ST_BIGDATA/Stereomics/USER/hanxiaoqing/data_deliv/v3/inpython/step2_data_deliv.py 
+python3 /hanxiaoqing/data_deliv/v3/inpython/step2_data_deliv.py 
